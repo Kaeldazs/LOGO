4aca91a1228d493c51b353d8a0d3950cd786ef2e
Kaeldazs <camille.sardin@gmail.com>
2016-12-08 11:03:44 +0100

    Interpreter v1

--- js/interpreter.js
+++ js/interpreter.js
@@ -2,57 +2,87 @@ var itpr = {
 	commands: {
 		AV: {
 			type: 'procedure',
-			reg: /^(AV)\s([0-9]+)/,
-			doc: 'AV pixels //The turtle moves pixels foward'
+			reg: /^(AV)\s([0-9]|:[a-zA-Z0-9_$]+)/,
+			doc: 'AV pixels //The turtle moves pixels foward',
+			exec: function() {
+				//
+			}
 		},
 		RE: {
 			type: 'procedure',
-			reg: /^(RE)\s([0-9]+)/,
-			doc: 'RE pixels //The turtle moves pixels backward'
+			reg: /^(RE)\s([0-9]|:[a-zA-Z0-9_$]+)/,
+			doc: 'RE pixels //The turtle moves pixels backward',
+			exec: function() {
+				//
+			}
 		},
 		TD: {
 			type: 'procedure',
-			reg: /^(TD)\s([0-9]+)/,
-			doc: 'TD degrees //The turtle turns degrees to the right'
+			reg: /^(TD)\s([0-9]|:[a-zA-Z0-9_$]+)/,
+			doc: 'TD degrees //The turtle turns degrees to the right',
+			exec: function() {
+				//
+			}
 		},
 		TG: {
 			type: 'procedure',
-			reg: /^(TG)\s([0-9]+)/,
-			doc: 'TG degrees //The turtle turns degrees to the left'
+			reg: /^(TG)\s([0-9]|:[a-zA-Z0-9_$]+)/,
+			doc: 'TG degrees //The turtle turns degrees to the left',
+			exec: function() {
+				//
+			}
 		},
 		FCC: {
 			type: 'procedure',
-			reg: /^(FCC)\s(#([[:xdigit:]]{6}|[[:xdigit:]]{3})+)/,
-			doc: 'FCC color //Change the trace color to color in RGB format as #FF0000 for red'
+			reg: /^(FCC)\s(#([[:xdigit:]]{6}|[[:xdigit:]]{3}))/,
+			doc: 'FCC color //Change the trace color to color in RGB format as #FF0000 for red',
+			exec: function() {
+				//
+			}
 		},
 		LC: {
 			type: 'procedure',
 			reg: /^(LC)/,
-			doc: 'LC //Pen up (no trace)'
+			doc: 'LC //Pen up (no trace)',
+			exec: function() {
+				//
+			}
 		},
 		BC: {
 			type: 'procedure',
 			reg: /^(BC)/,
-			doc: 'BC //Pen down (trace active)'
+			doc: 'BC //Pen down (trace active)',
+			exec: function() {
+				//
+			}
 		},
 		VE: {
 			type: 'procedure',
 			reg: /^(VE)/,
-			doc: 'VE //Clears the screen and put the turtle at the center, facing upwards'
+			doc: 'VE //Clears the screen and put the turtle at the center, facing upwards',
+			exec: function() {
+				//
+			}
 		},
 		CT: {
 			type: 'procedure',
 			reg: /^(CT)/,
-			doc: 'CT //Hide the turtle'
+			doc: 'CT //Hide the turtle',
+			exec: function() {
+				//
+			}
 		},
 		MT: {
 			type: 'procedure',
 			reg: /^(MT)/,
-			doc: 'MT //Show the turtle'
+			doc: 'MT //Show the turtle',
+			exec: function() {
+				//
+			}
 		},
 		REPETE: {
 			type: 'structure',
-			reg: /^REPETE\s([0-9])\s\[([^\]]+)\]/,
+			reg: /^REPETE\s([0-9]|:[a-zA-Z0-9_$])\s\[([^\]]+)\]/,
 			doc: 'REPETE x [commands] //Do the commands x times',
 			store: function(match) {
 				var iterations = match[1];
@@ -60,6 +90,9 @@ var itpr = {
 				for (var i = 0; i < iterations; i++) {
 					itpr.run(commands);
 				}
+			},
+			exec: function() {
+				//
 			}
 		},
 		POUR: {
@@ -69,16 +102,28 @@ var itpr = {
 			store: function(match) {
 				var argsRegex = /(\s:([a-zA-Z0-9_$]+))/g;
 				var args = match[0].match(argsRegex);
+				var regVar = '(?:'+ match[2] +')';
+				var regFunction = '^(' + match[2] + ')';
 				for (var i in args) {
-					args[i] = args[i].replace(/^\s:/g, '');
+					args[i] = args[i].replace(/^\s/g, '');
+
+					regVar += '(?:\\\s(\\\d+))';
+					regFunction += '(?:\\\s(\\\d+))';
 				}
 				itpr.commands[match[2] + ''] = {
 					type: 'function',
-					reg: new RegExp('^(' + match[2] + ')', 'i'),
+					reg: new RegExp(regFunction),
 					buffer: [],
-					store: function() {
-						for (var i = 0; i < this.buffer.length; i++) {
-							itpr.buffer[itpr.buffer.length] = this.buffer[i];
+					localVar: args,
+					store: function(match) {
+						var i;
+						for (i = 0; i < this.buffer.length; i++) {
+							var argsStr = this.buffer[i][1];
+							for (var j = 0; j < this.localVar.length; j++) {
+								var r = new RegExp('^'+ this.localVar[j] +'$', 'g')
+								argsStr = argsStr.replace(r, match[j + 2]);
+							}
+							itpr.buffer[itpr.buffer.length] = [this.buffer[i][0], argsStr];
 						}
 					}
 				}
@@ -88,20 +133,28 @@ var itpr = {
 		FIN: {
 			type: 'procedure',
 			reg: /^(FIN)/,
-			doc: '',
+			doc: 'FIN //End of capture',
 			store: function(match) {
 				itpr.capture = false
+			},
+			exec: function() {
+				//
 			}
 		}
 	},
 	buffer: [],
 	store: function(match) {
 		if (itpr.capture) {
-			itpr.commands[itpr.capture].buffer[itpr.commands[itpr.capture].buffer.length] = [match[1], match[2]]
+			if (itpr.commands[itpr.capture]) {
+				itpr.commands[itpr.capture].buffer[itpr.commands[itpr.capture].buffer.length] = [match[1], match[2]];
+			}
 		}
 		else {
 			itpr.buffer[itpr.buffer.length] = [match[1], match[2]];
 		}
+	},
+	clear: function() {
+		itpr.buffer = [];
 	},
 	capture: false,
 	run: function(str) {

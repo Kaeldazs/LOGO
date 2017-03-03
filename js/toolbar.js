92a178b819532b87b38d751990b307d7abf81c2a
Camille Sardin <camille.sardin@gmail.com>
2017-03-03 10:34:58 +0100

    optimizations

--- js/toolbar.js
+++ js/toolbar.js
@@ -1,57 +1,50 @@
 Toolbar = function() {
-	var _this = this;
+	var _ = this;
 	this.height = 40;
 	this.btn = {};
+	this.el;
 	this.create = function() {
-		var tb = document.createElement('div');
-		tb.id = 'toolbar';
-		tb.style.bottom = shell.height + 'px';
-		tb.style.height = _this.height + 'px';
+		_.el = document.createElement('div');
+		_.el.id = 'toolbar';
+		_.el.style.bottom = shell.height + 'px';
+		_.el.style.height = _.height + 'px';
 
-		_this.btn.playPause = document.createElement('img');
-		_this.btn.playPause.src = 'img/play.png'
-		_this.btn.playPause.style.marginTop = (_this.height - 24)/2 + 'px';
-		_this.btn.playPause.onclick = function() {
+		_.genBtnImg('playPause', 'img/play.png', function() {
 			itpr.toggle();
-		};
-		tb.appendChild(_this.btn.playPause);
+		});
 
-		_this.btn.clear = document.createElement('img');
-		_this.btn.clear.src = 'img/clear.png'
-		_this.btn.clear.style.marginTop = (_this.height - 24)/2 + 'px';
-		_this.btn.clear.onclick = function() {
+		_.genBtnImg('clear', 'img/clear.png', function() {
 			itpr.clear(true);
-		};
-		tb.appendChild(_this.btn.clear);
+		});
 
-		_this.btn.slow = document.createElement('img');
-		_this.btn.slow.src = 'img/slow.png'
-		_this.btn.slow.style.marginTop = (_this.height - 24)/2 + 'px';
-		_this.btn.slow.onclick = function() {
-			_this.activeBtn(_this.btn.slow, 1);
-		};
-		tb.appendChild(_this.btn.slow);
+		_.genBtnImg('slow', 'img/slow.png', function() {
+			_.activeBtn(_.btn.slow, 1);
+		});
 
-	_this.btn.normal = document.createElement('img');
-		_this.btn.normal.src = 'img/normal.png'
-		_this.btn.normal.style.marginTop = (_this.height - 24)/2 + 'px';
-		_this.btn.normal.onclick = function() {
-			_this.activeBtn(_this.btn.normal, 6);
-		};
-		tb.appendChild(_this.btn.normal);
+		_.genBtnImg('normal', 'img/normal.png', function() {
+			_.activeBtn(_.btn.normal, 6);
+		});
+		_.activeBtn(_.btn.normal, 6);
 
-	_this.btn.fast = document.createElement('img');
-		_this.btn.fast.src = 'img/fast.png'
-		_this.btn.fast.style.marginTop = (_this.height - 24)/2 + 'px';
-		_this.btn.fast.onclick = function() {
-			_this.activeBtn(_this.btn.fast, 0);
-		};
-		tb.appendChild(_this.btn.fast);
+		_.genBtnImg('fast', 'img/fast.png', function() {
+			_.activeBtn(_.btn.fast, 0);
+		});
 
-		document.body.appendChild(tb);
+		document.body.appendChild(_.el);
 	}
 
-	_this.activeBtn = function(el, speed) {
+	_.genBtnImg = function(name, src, func) {
+		_.btn[name] = document.createElement('img');
+		_.btn[name].src = src;
+		_.btn[name].style.marginTop = (_.height - 24)/2 + 'px';
+		_.btn[name].onclick = func;
+		_.btn[name].ondragstart = function(e) {
+			e.preventDefault();
+		};
+		_.el.appendChild(_.btn[name]);
+	};
+
+	_.activeBtn = function(el, speed) {
 		removeClassName('active', '#toolbar .active');
 		el.classList.add('active');
 		itpr.speed(speed);
